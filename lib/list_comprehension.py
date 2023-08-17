57584e91f25adcb581861516a1a3d8be661c7c06
Winny <winny.chelangat2@student.moringaschool.com>
2023-08-17 20:09:04 +0300

    Completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [num for num in num_list if num % 2 == 0]
+    return evens
 
 def make_exclamation(sentence_list):
-    pass
+    exclamations = [sentence + "!" for sentence in sentence_list]
+    return exclamations
